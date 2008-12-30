7acc54c048e1a8964d66c0fd04d5bc1ba9574022
Thom Simmons <thom@sliceofpi.org>
2008-12-29 19:02:39 -0600

    Modified String quotations

--- test.py
+++ test.py
@@ -4,5 +4,9 @@ class TestPy(object):
   def __init__(self):
     test_regex = r'[0-9]{2,4}.\{'
     test_single_quote = 'single quote string'
+    test_single_quote = u'single quote string'
+    test_single_quote = '''single quote string'''
     test_double_quote = "double quote string"
     test_double_quote = u"double quote string"
+    test_double_quote = """double quote string"""
+    test_double_quote = u"""double quote string"""
