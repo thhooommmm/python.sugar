72979fccdab7de390317c0b383f8471b04620a17
Thom Simmons <thom@sliceofpi.org>
2008-12-29 18:48:39 -0600

    Updating some regexes

--- test.py
+++ test.py
@@ -4,4 +4,5 @@ class TestPy(object):
   def __init__(self):
     test_regex = r'[0-9]{2,4}.\{'
     test_single_quote = 'single quote string'
-    
+    test_double_quote = "double quote string"
+    test_double_quote = u"double quote string"
