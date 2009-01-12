1db3d5da6c061109108359ba23a9703bbf27fd1a
Thom Simmons <thom@sliceofpi.org>
2009-01-11 23:02:44 -0600

    Lots of updates. No, codefolding isn't here yet. I need it to be usable more than I need folding right now.

---
+++ tests/test.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+
+import sys,io
+from sys import test
+from django.db import db.models, forms.forms, contrib.* as testOne, somwrhinf as EHWKsd
+
+try:
+  return True
+except Exception,e:
+  pass
+finally:
+  fp.close()
+
+class TestPy(object)
+	"""
+		Class Documentation Comment.
+	"""
+	#hello world
+  def __init__(self, alpha, beta, gamma, __uh):
+  	'''
+			Function Documentation Comment.
+		'''
+		
+    test_regex = r'[0-9]{2,4}.\{'
+    test_single_quote_nobody = ''
+    test_single_quote = 'single quote string'
+    test_single_quote = u'single quote string'
+    test_single_quote = '''single
+                           quote
+                           string'''
+    test_single_quote = u'''single
+                           quote
+                           string'''
+    test_double_quote = "double quote string"
+    test_double_quote_nobody = ""
+    test_double_quote = u"double quote string"
+    test_double_quote = """double
+                           quote
+                           string"""
+    test_double_quote = u"""double 
+                            quote 
+                            string"""
+    return True
+
+    def __test(self):
+      return None
+
+    def test2(self, object):
+      pass
+
+
+
+
+
+
+
+
+class Test:
+    def __init__(self):
+      return None
+     
+    
+    
+  
